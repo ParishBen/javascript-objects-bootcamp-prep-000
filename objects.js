721cee5a69e75e62390dd053cfbe48a384875aa6
parishben13@gmail.com <parishben13@gmail.com>
2018-04-17 03:25:22 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist= {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'}
+function updatePlaylist(playlist, Phil Ochs, Heres to the State of Mississippi){
+playlist['Phil Ochs']="Here's to the State of Mississippi";
+return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  return playlist
+}
